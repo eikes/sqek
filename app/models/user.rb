50e1211d17bc84d943459e21780cadaa5155b574
Eike Send <eike.send@gmail.com>
2015-01-05 17:43:34 +0100

    disallow user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  #
+  # Removed default modules:
+  # :registerable
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end

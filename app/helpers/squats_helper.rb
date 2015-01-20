db257b2b13255b41c0862aa1732da9854398a93b
Eike Send <eike.send@gmail.com>
2015-01-20 18:55:25 +0100

    show evicted icon if squat is no longer occupied

--- app/helpers/squats_helper.rb
+++ app/helpers/squats_helper.rb
@@ -1,7 +1,11 @@
 module SquatsHelper
 
   def squat_get_icon(squat)
-    asset_path( "icons/haus_#{(squat.periods.first.start_year/10*10).to_s}er.png" )
+    if squat.periods.last.end_year.nil?
+      asset_path( "icons/haus_#{(squat.periods.first.start_year/10*10).to_s}er.png" )
+    else
+      asset_path( "icons/haus_geraumt.png" )
+    end
   end
 
 end

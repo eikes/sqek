240f44bb8c9b365645ece188d1fed1321df09123
Eike Send <eike.send@gmail.com>
2015-02-24 23:31:07 +0100

    add simple seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create(
+  email: "a@bc.de",
+  password: "123123123",
+  role: "admin"
+)
+
+berlin = City.create(
+  name: 'Berlin',
+  lat: 52.516,
+  lng: 13.383
+)
+
+kopi = Squat.new(
+  name: "Köpi",
+  city: berlin,
+  lat: 52.5077,
+  lng: 13.426
+)
+
+kopi.save(validate: false)
+
+Period.create(
+  start_year: 1990,
+  squat: kopi
+)
+
+kopi.save
+
+tommy = Squat.new(
+  name: "Tommy-Weisbecker-Haus",
+  city: berlin,
+  lat: 52.5013,
+  lng: 13.389
+)
+
+tommy.save(validate: false)
+
+Period.create(
+  start_year: 1973,
+  start_month: 3,
+  squat: tommy
+)
+
+tommy.save

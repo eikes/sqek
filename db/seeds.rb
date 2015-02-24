e5ba1ce009da3101b53ae125f008dda674cf3305
Eike Send <eike.send@gmail.com>
2015-02-25 00:24:23 +0100

    improve tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 admin = User.create(
   email: "a@bc.de",
@@ -14,39 +9,34 @@ admin = User.create(
 
 berlin = City.create(
   name: 'Berlin',
+  slug: 'berlin',
   lat: 52.516,
   lng: 13.383
 )
 
 kopi = Squat.new(
   name: "Köpi",
+  slug: 'kopi',
   city: berlin,
   lat: 52.5077,
   lng: 13.426
 )
-
 kopi.save(validate: false)
-
 Period.create(
   start_year: 1990,
   squat: kopi
 )
 
-kopi.save
-
 tommy = Squat.new(
   name: "Tommy-Weisbecker-Haus",
+  slug: "tommy-weisbecker-haus",
   city: berlin,
   lat: 52.5013,
   lng: 13.389
 )
-
 tommy.save(validate: false)
-
 Period.create(
   start_year: 1973,
   start_month: 3,
   squat: tommy
 )
-
-tommy.save

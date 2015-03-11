1357c2aa0a11332cc410f18e8f7299a834151565
Eike Send <eike.send@gmail.com>
2015-03-11 23:39:39 +0100

    Add icons for evicted squats

--- app/helpers/squats_helper.rb
+++ app/helpers/squats_helper.rb
@@ -4,7 +4,7 @@ module SquatsHelper
     if squat.periods.last.end_year.nil?
       asset_path( "icons/haus_#{(squat.periods.first.start_year/10*10).to_s}er.png" )
     else
-      asset_path( "icons/haus_geraumt.png" )
+      asset_path( "icons/haus_evicted_#{(squat.periods.first.start_year/10*10).to_s}er.png" )
     end
   end
 

82efe81405a92bf1aa1a76f07fc9c52d8f8d5faa
Susanne Dewein <susanne.dewein@gmail.com>
2015-04-14 14:48:44 +0200

    make sure squats for pictures are unique

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,10 @@
 class Picture < ActiveRecord::Base
+  default_scope{ order('created_at DESC')}
   mount_uploader :picture, PictureUploader
 
-  has_and_belongs_to_many :squats
+  has_and_belongs_to_many :squats, uniq: true
   has_many :cities, through: :squats
 
-  validates :title, :description, :picture, presence: true
+  validates :title, :picture, :squats, presence: true
 
 end

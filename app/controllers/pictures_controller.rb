09e28bf71ca59391081f882158a5f57fd42e6293
Susanne Dewein <susanne.dewein@gmail.com>
2015-04-14 14:48:05 +0200

    Fix error messages and redirects

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -23,19 +23,21 @@ class PicturesController < ApplicationController
     @picture.squats = Squat.find(params[:picture][:squat_ids].delete_if { |squat| squat.empty? })
     if @picture.save
       flash[:notice] = "Picture created"
+      redirect_to city_picture_path(@city, @picture)
     else
-      flash[:error] = "Picture could not be saved"
+      flash[:error] = "Picture could not be saved: #{@picture.errors.full_messages.join('. ')}"
+      render 'new'
     end
-    redirect_to city_pictures_path(@city)
   end
 
   def update
     if @picture.update(picture_params)
       flash[:notice] = "Picture updated"
+      redirect_to city_pictures_path(@city)
     else
-      flash[:error] = "Picture could not be updated"
+      flash[:error] = "Picture could not be updated: #{@picture.errors.full_messages.join('. ')}"
+      render 'edit'
     end
-    redirect_to city_pictures_path(@city)
   end
 
   def destroy

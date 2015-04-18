facbed15bfeb91bdfae98fb1bc3301a4c9a16ae8
Eike Send <eike.send@gmail.com>
2015-04-19 00:36:46 +0200

    move pictures to description partial

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -33,15 +33,15 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [100, 100]
+    process resize_to_fill: [100, 100]
   end
 
   version :index do
-    process :resize_to_limit => [300, 300]
+    process resize_to_limit: [300, 300]
   end
 
   version :show do
-    process :resize_to_limit => [500, 500]
+    process resize_to_limit: [500, 500]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.

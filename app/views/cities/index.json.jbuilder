89aa26e270be59316ce980fbe9677e56c0bd2a51
Susanne Dewein <susanne.dewein@gmail.com>
2015-07-09 17:44:56 +0200

    add external_url to city

--- app/views/cities/index.json.jbuilder
+++ app/views/cities/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@cities) do |city|
-  json.extract! city, :id, :name, :body, :latlng
+  json.extract! city, :id, :name, :body, :latlng, :external_url
   json.url city_squats_url(city)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226172633) do
+ActiveRecord::Schema.define(version: 20150709151755) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.text     "body"
     t.float    "lat"
     t.float    "lng"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "slug"
+    t.string   "external_url"
   end
 
   add_index "cities", ["slug"], name: "index_cities_on_slug", unique: true

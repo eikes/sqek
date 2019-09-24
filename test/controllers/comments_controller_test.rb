047bd0ed52738211d10e5d280f501771ca72ae5f
Eike Send <eike.send@gmail.com>
2019-09-24 10:35:53 +0200

    Removes deprecation warnings for tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
+  include Devise::Test::ControllerHelpers
 
   setup do
     @city = cities(:berlin)
--- test/controllers/squats_controller_test.rb
+++ test/controllers/squats_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SquatsControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
+  include Devise::Test::ControllerHelpers
 
   setup do
     @city = cities(:berlin)
